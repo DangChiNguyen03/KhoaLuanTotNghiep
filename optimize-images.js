4ed0bccd7cb9a0170a30f268a551c393804f2ca7
DangChiNguyen03 <nguyenchinguyen02003@gmail.com>
2025-12-06 00:12:32 +0700

    toiuuanh

---
+++ optimize-images.js
@@ -0,0 +1,22 @@
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
+const inputDir = path.join(__dirname, "src/images");
+const outputDir = path.join(__dirname, "dist/images");
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+fs.readdirSync(inputDir).forEach((file) => {
+  const inputPath = path.join(inputDir, file);
+  const outputPath = path.join(outputDir, file);
+
+  sharp(inputPath)
+    .resize(1200)
+    .jpeg({ quality: 80 })
+    .toFile(outputPath)
+    .then(() => console.log("✔ Optimized:", file))
+    .catch((err) => console.error("✖ Error:", file, err));
+});
